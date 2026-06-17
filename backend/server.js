6b962a989dd57f4c21226e51d7e13a768ae9748a
Gokul <gokulofficial556@gmail.com>
2026-06-17 16:12:04 +0530

    mongodb connection established

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
-console.log("ResolveIt")
+
+
+dotenv.config();
+connectDB();
+const app =express();
+app.use(cors());
+app.use(express.json);
+
+
+
